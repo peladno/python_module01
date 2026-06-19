5e2d0848dabd5f508585f7a6725a0bc3ce6dce28
Javier Pérez <95249621+peladno@users.noreply.github.com>
2026-06-19 03:24:36 +0000

    fix(plant_grow): "refact code, main looks cleaner"

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -4,39 +4,41 @@ class Plant:
         name: str,
         height: float,
         age: int,
-        age_a_day: int = 1,
-        grow_per_day: float = .1,
     ):
         self.name = name
         self.height = float(height)
         self.age = int(age)
-        self.age_a_day = int(age_a_day)
-        self.grow_per_day = float(grow_per_day)
 
     def show(self):
         print(f"{self.name}: {round(self.height, 1)}, {self.age} days old")
 
     def grow(self):
-        self.height *= (1 + self.grow_per_day)
+        self.height *= 1.1
         return self.height
 
     def age_day(self):
-        self.age += self.age_a_day
+        self.age += 1
         return self.age
 
     def simulate_day(self):
         self.grow()
         self.age_day()
 
+    def simulate_days(self, days):
+        initial = self.height
+        for day in range(days):
+            print(f"=== Day {day} ===")
+            self.simulate_day()
+            self.show()
+        return self.height - initial
+
+
 
 def main():
     plant1 = Plant("Rose", 50, 10)
-
-    for days in range(1, 8):
-        print(f"day{days}")
-        plant1.simulate_day()
-
-    plant1.show()
+    print("=== Garden Plant Growth ===")
+    diff = plant1.simulate_days(7)
+    print(f"Growth this week: {diff:.1f}cm")
 
 
 if __name__ == "__main__":
